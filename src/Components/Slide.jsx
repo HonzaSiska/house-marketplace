2f66bb76c5a54d79ebe69e257c1b17710e8d3b6c
HonzaSiska <siskajan@hotmail.com>
2022-06-14 19:51:58 +0200

    fade effect added to slide show

--- src/Components/Slide.jsx
+++ src/Components/Slide.jsx
@@ -3,18 +3,21 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import { EffectFade } from 'swiper';
+
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade';
 
 function Slide({images}) {
     console.log(images[0])
     return (
         <Swiper
         // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        modules={[EffectFade,Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
         slidesPerView={1}
         navigation
@@ -22,6 +25,7 @@ function Slide({images}) {
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
+        effect="fade"
       >
         {/* <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
